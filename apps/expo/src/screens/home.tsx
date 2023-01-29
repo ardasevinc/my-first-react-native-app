a1ce4cd626b3cf394c63bc576f8b4ddcbd188c64
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-01-29 13:39:33 +0300

    chore: prettier

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useAuth } from "@clerk/clerk-expo";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { FlashList } from "@shopify/flash-list";
-import type { inferProcedureOutput } from "@trpc/server";
-import type { AppRouter } from "@acme/api";
+import { Button, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useAuth } from '@clerk/clerk-expo';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlashList } from '@shopify/flash-list';
+import type { inferProcedureOutput } from '@trpc/server';
+import type { AppRouter } from '@acme/api';
 
-import { trpc } from "../utils/trpc";
+import { trpc } from '../utils/trpc';
 
 const SignOut = () => {
   const { signOut } = useAuth();
   return (
-    <View className="rounded-lg border-2 border-gray-500 p-4">
+    <View className='rounded-lg border-2 border-gray-500 p-4'>
       <Button
-        title="Sign Out"
+        title='Sign Out'
         onPress={() => {
           signOut();
         }}
@@ -24,12 +24,12 @@ const SignOut = () => {
 };
 
 const PostCard: React.FC<{
-  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
+  post: inferProcedureOutput<AppRouter['post']['all']>[number];
 }> = ({ post }) => {
   return (
-    <View className="rounded-lg border-2 border-gray-500 p-4">
-      <Text className="text-xl font-semibold text-[#cc66ff]">{post.title}</Text>
-      <Text className="text-white">{post.content}</Text>
+    <View className='rounded-lg border-2 border-gray-500 p-4'>
+      <Text className='text-xl font-semibold text-[#cc66ff]'>{post.title}</Text>
+      <Text className='text-white'>{post.content}</Text>
     </View>
   );
 };
@@ -40,25 +40,28 @@ const CreatePost: React.FC = () => {
     async onSuccess() {
       await utils.post.all.invalidate();
     },
+    async onError(e) {
+      console.error(e);
+    },
   });
 
-  const [title, onChangeTitle] = React.useState("");
-  const [content, onChangeContent] = React.useState("");
+  const [title, onChangeTitle] = React.useState('');
+  const [content, onChangeContent] = React.useState('');
 
   return (
-    <View className="flex flex-col border-t-2 border-gray-500 p-4">
+    <View className='flex flex-col border-t-2 border-gray-500 p-4'>
       <TextInput
-        className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
+        className='mb-2 rounded border-2 border-gray-500 p-2 text-white'
         onChangeText={onChangeTitle}
-        placeholder="Title"
+        placeholder='Title'
       />
       <TextInput
-        className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
+        className='mb-2 rounded border-2 border-gray-500 p-2 text-white'
         onChangeText={onChangeContent}
-        placeholder="Content"
+        placeholder='Content'
       />
       <TouchableOpacity
-        className="rounded bg-[#cc66ff] p-2"
+        className='rounded bg-[#cc66ff] p-2'
         onPress={() => {
           mutate({
             title,
@@ -66,7 +69,7 @@ const CreatePost: React.FC = () => {
           });
         }}
       >
-        <Text className="font-semibold text-white">Publish post</Text>
+        <Text className='font-semibold text-white'>Publish post</Text>
       </TouchableOpacity>
     </View>
   );
@@ -77,20 +80,20 @@ export const HomeScreen = () => {
   const [showPost, setShowPost] = React.useState<string | null>(null);
 
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <View className="h-full w-full p-4">
-        <Text className="mx-auto pb-2 text-5xl font-bold text-white">
-          Create <Text className="text-[#cc66ff]">T3</Text> Turbo
+    <SafeAreaView className='bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]'>
+      <View className='h-full w-full p-4'>
+        <Text className='mx-auto pb-2 text-5xl font-bold text-white'>
+          Create <Text className='text-[#cc66ff]'>T3</Text> Turbo
         </Text>
 
-        <View className="py-2">
+        <View className='py-2'>
           {showPost ? (
-            <Text className="text-white">
-              <Text className="font-semibold">Selected post:</Text>
+            <Text className='text-white'>
+              <Text className='font-semibold'>Selected post:</Text>
               {showPost}
             </Text>
           ) : (
-            <Text className="font-semibold italic text-white">
+            <Text className='font-semibold italic text-white'>
               Press on a post
             </Text>
           )}
@@ -99,7 +102,7 @@ export const HomeScreen = () => {
         <FlashList
           data={postQuery.data}
           estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
+          ItemSeparatorComponent={() => <View className='h-2' />}
           renderItem={(p) => (
             <TouchableOpacity onPress={() => setShowPost(p.item.id)}>
               <PostCard post={p.item} />
--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,5 +1,5 @@
-import { router, publicProcedure, protectedProcedure } from "../trpc";
-import { z } from "zod";
+import { router, publicProcedure, protectedProcedure } from '../trpc';
+import { z } from 'zod';
 
 export const postRouter = router({
   all: publicProcedure.query(({ ctx }) => {
@@ -11,6 +11,8 @@ export const postRouter = router({
   create: protectedProcedure
     .input(z.object({ title: z.string(), content: z.string() }))
     .mutation(({ ctx, input }) => {
+      console.log(`input ${input}`);
+      console.log(`context ${ctx}`);
       return ctx.prisma.post.create({ data: input });
     }),
 });
