39314a4741dbbcb65076849ca3b86fc82249ea05
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-01-29 13:38:20 +0300

    feat: add clerk public key

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_dG91Y2hpbmctbmV3dC00MS5jbGVyay5hY2NvdW50cy5kZXYk";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
